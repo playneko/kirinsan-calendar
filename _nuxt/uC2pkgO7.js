3839b781c82e46ec24999ef858edd613b1b49fca
Playneko <playneko@outlook.com>
2024-10-01 00:36:58 +0900

    0e5affc9

---
+++ _nuxt/uC2pkgO7.js
@@ -0,0 +1,1 @@
+import{_ as o,v as t,x as n,t as r}from"./DX08fkZ-.js";const s={};function l(a,e){return r(),t("div",null,e[0]||(e[0]=[n("h1",null,"Not Mobile",-1)]))}const _=o(s,[["render",l]]);export{_ as default};
